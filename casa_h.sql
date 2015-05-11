caa86c7d29d4c864f3af0041721e62a9bca6daba
Man-gel <night_captain@hotmail.com>
2015-05-10 21:36:35 -0500

    Se modifica la base de datos y casi se termina el proyecto

---
+++ casa_h.sql
@@ -0,0 +1,162 @@
+-- MySQL dump 10.14  Distrib 5.5.40-MariaDB, for Win64 (x86)
+--
+-- Host: localhost    Database: casah
+-- ------------------------------------------------------
+-- Server version	5.5.40-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE IF NOT EXISTS `casah` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `casah`;
+
+--
+-- Table structure for table `beneficiarios`
+--
+
+DROP TABLE IF EXISTS `beneficiarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `beneficiarios` (
+  `id_beneficiario` int(5) NOT NULL AUTO_INCREMENT,
+  `bnombre` varchar(50) NOT NULL,
+  `tipo` varchar(65) DEFAULT "NO DISPONIBLE",
+  PRIMARY KEY (`id_beneficiario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `beneficiarios`
+--
+
+LOCK TABLES `beneficiarios` WRITE;
+/*!40000 ALTER TABLE `beneficiarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `beneficiarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `existencias`
+--
+
+DROP TABLE IF EXISTS `existencias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `existencias` (
+  `id_producto` int(4) NOT NULL,
+  `cantidad` float DEFAULT 0,
+  `monto` float DEFAULT 0,
+  `descripcion` varchar(50) DEFAULT "NO DISPONIBLE",
+  `fecha_actualizacion` datetime NOT NULL,
+  `id_beneficiario` int(5) NOT NULL,
+  `id_proveedor` int(4) NOT NULL,
+  `usuario` varchar(35) NOT NULL,
+  `movimiento` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `existencias`
+--
+
+LOCK TABLES `existencias` WRITE;
+/*!40000 ALTER TABLE `existencias` DISABLE KEYS */;
+/*!40000 ALTER TABLE `existencias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto` (
+  `id_producto` int(4) NOT NULL AUTO_INCREMENT,
+  `pnombre` varchar(40) NOT NULL,
+  `descripcion` varchar(60) DEFAULT "NO DISPONIBLE",
+  `cantidad` float DEFAULT 0,
+  `monto` float DEFAULT 0,
+PRIMARY KEY (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedores`
+--
+
+DROP TABLE IF EXISTS `proveedores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proveedores` (
+  `id_proveedor` int(4) NOT NULL AUTO_INCREMENT,
+  `pvnombre` varchar(50) NOT NULL,
+  `domicilio` varchar(45) DEFAULT "NO DISPONIBLE",
+  PRIMARY KEY (`id_proveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedores`
+--
+
+LOCK TABLES `proveedores` WRITE;
+/*!40000 ALTER TABLE `proveedores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proveedores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id_usuario` int(3) NOT NULL AUTO_INCREMENT,
+  `user` varchar(35) NOT NULL,
+  `unombre` varchar(45) NOT NULL,
+  `permisos` varchar(25) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE KEY `user` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'root','DEFAULT','Administrador','12345');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-03 22:07:51
